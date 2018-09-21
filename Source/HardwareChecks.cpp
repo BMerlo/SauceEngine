2f078e3d9279d9cede3452afe68e7fdc5fb4c7d6
Peter Tu <peter.tu222@gmail.com>
2018-09-21 09:10:35 -0400

    CheckRAM and CheckCPU
    Added a function that checks the user's RAM and CPU.

--- Source/HardwareChecks.cpp
+++ Source/HardwareChecks.cpp
@@ -1,8 +1,40 @@
 // Hardware checks for engine starts.
 #include "HardwareChecks.h"
 #include "Windows.h"
+#include <wchar.h>
+#include <iostream>
+using namespace std;
 
 
+inline void CheckRAM()
+{
+	MEMORYSTATUSEX statex;
+	statex.dwLength = sizeof(statex);
+	GlobalMemoryStatusEx(&statex);
+	cout << "Total System Memory: " << (statex.ullTotalPhys / 1024) / 1024 << "MB" << endl;
 
-
+}
+inline DWORD ReadCPUSpeed() {
+	DWORD BufSize = sizeof(DWORD);
+	DWORD dwMHz;
+	DWORD type = REG_DWORD;
+	HKEY hKey;
+	// open the key where the proc speed is hidden:
+	long lError = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+		L"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0",
+		0,
+		KEY_READ,
+		&hKey);
+	if (lError == ERROR_SUCCESS) {
+		// query the key:
+		RegQueryValueEx(hKey,
+			L"MHz",
+			NULL,
+			&type,
+			(LPBYTE)&dwMHz,
+			&BufSize);
+	}
+	cout << "CPU Speed: ~" << dwMHz / 1000000 << "MHz" << endl;
+	return dwMHz;
+}
 
